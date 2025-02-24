665a1307dbad37d8e7b45ccd55a80d4a7254d9ff
Dmitriy Safronov <zimniy@cyberbrain.pw>
2025-02-24 14:33:08 +0400

    feature: code + reqs
    BREAKING CHANGE
    
    Signed-off-by: Dmitriy Safronov <zimniy@cyberbrain.pw>

---
+++ telegram_file_sender/__main__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from os import environ
+import requests
+
+
+def get_env_var(variable_name):
+    result = environ.get(variable_name)
+    if result is None or result == "":
+        raise Exception(f"The environment variable [{variable_name}] is required!")
+    return result
+
+
+def main():
+    TELEGRAM_BOT_TOKEN = get_env_var("TELEGRAM_BOT_TOKEN")
+    TELEGRAM_FILE_NAME = get_env_var("TELEGRAM_FILE_NAME")
+    TELEGRAM_CHAT_ID = int(get_env_var("TELEGRAM_CHAT_ID"))
+    TELEGRAM_CHAT_MESSAGE = get_env_var("TELEGRAM_CHAT_MESSAGE")
+
+    try:
+        response = requests.post(f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendDocument', json={'chat_id': TELEGRAM_CHAT_ID, 'document': TELEGRAM_FILE_NAME, 'caption': TELEGRAM_CHAT_MESSAGE})
+        print(response.text)
+    except Exception as e:
+        print(e)
+
+if __name__ == "__main__":
+    main()
